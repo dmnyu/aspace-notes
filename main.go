114629f2c708541f0bbcd92b18784b2714f0cb40
Don Mennerich <don.mennerich@nyu.edu>
2025-05-16 15:09:15 -0400

    adding updating flags and help message

--- main.go
+++ main.go
@@ -11,21 +11,41 @@ import (
 )
 
 var (
-	env    string
-	config string
-	repoID int
+	env        string
+	config     string
+	repoID     int
+	helpmsg    bool
+	outputFile string
 )
 
 func init() {
 	flag.StringVar(&env, "env", "", "")
 	flag.StringVar(&config, "config", "", "")
-	flag.IntVar(&repoID, "repo-id", 0, "")
+	flag.IntVar(&repoID, "repo-id", 2, "")
+	flag.BoolVar(&helpmsg, "help", false, "")
+	flag.StringVar(&outputFile, "output-file", "output.csv", "")
+}
+
+func help() {
+	fmt.Println("Usage: aspace-notes [options]")
+	fmt.Println("Options:")
+	fmt.Println("  --config path/to/ go-aspace config file, Mandatory")
+	fmt.Println("  --env the environment to run program against, Mandatory")
+	fmt.Println("  --repo-id the id of the repository to run program against, default: `2`")
+	fmt.Println("  --output-file the path/to the output file, default: output.csv")
+	fmt.Println("  --help print this help message")
+	os.Exit(0)
 }
 
 func main() {
 	fmt.Println("aspace-notes, v0.1.0")
 	//parse flags
 	flag.Parse()
+
+	//check for help
+	if helpmsg {
+		help()
+	}
 
 	//get client
 	client, err := aspace.NewClient(config, env, 20)
@@ -34,11 +54,13 @@ func main() {
 	}
 
 	//create output file
-	outfile, err := os.Create("out.csv")
+	outfile, err := os.Create(outputFile)
 	if err != nil {
 		panic(err)
 	}
 	defer outfile.Close()
+
+	//create a csv writer
 	writer := csv.NewWriter(outfile)
 	writer.Write([]string{"ID", "TITLE", "NOTE"})
 
@@ -72,8 +94,8 @@ func main() {
 
 			//gather the notes
 			for _, note := range selectedNotes {
-				for _, v := range note.Subnotes {
-					collectedNotes = append(collectedNotes, strings.ReplaceAll(v.Content, "\n", ""))
+				for _, subnote := range note.Subnotes {
+					collectedNotes = append(collectedNotes, strings.ReplaceAll(subnote.Content, "\n", ""))
 				}
 			}
 
